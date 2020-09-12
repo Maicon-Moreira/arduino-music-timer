36f0e2ce1a42a1a18de03218814ad1f597d63af3
Maicon-Moreira <maiconmoreirapira@gmail.com>
2020-09-12 18:15:51 -0300

    base done

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,11 +1,49 @@
+int entriesCounter = 0;
+long int microsInterval = 0;
+bool isClicking = false;
+long int lastClick = 0;
+long int clickTimes[1000];
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
   pinMode(52, INPUT);
-
+  pinMode(22, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int input = digitalRead(52);
 
+  if (input == 1 && !isClicking){
+    isClicking = true;
+
+    double currentTime = micros();
+    double clickDelay = currentTime - lastClick;
+
+    clickTimes[entriesCounter] = clickDelay;
+
+    entriesCounter ++;
+    lastClick = currentTime;
+
+    long int total = 0;
+    for (int i = 1; i < entriesCounter; i ++){
+       total += clickTimes[i];
+    }
+    microsInterval = total/(entriesCounter - 1);
+
+    Serial.println("Clique");
+    Serial.println(microsInterval);
+  }
+  
+  if (input == 0){
+    isClicking = false;  
+  }
+
+
+  if ((micros() - lastClick) % microsInterval < microsInterval*0.5){
+    digitalWrite(22, HIGH);  
+  }
+  else {
+    digitalWrite(22, LOW); 
+  }
 }
